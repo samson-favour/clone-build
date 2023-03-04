5ed27a96b98170a8f29bf410a28616fdd78c8940
samson-favour <favoursamson347@gmail.com>
2023-03-04 08:09:35 +0100

    changes added

--- next.config.js
+++ next.config.js
@@ -5,16 +5,15 @@ module.exports = {
   images: {
     domains: ["links.papareact.com", "ogle.ng", "https://ogle.ng"],
   },
- 
+
   env: {
     mapbox_key:
       "pk.eyJ1Ijoic2FtZmF2IiwiYSI6ImNreTB2cHF1cTA1bGoyb2xsY3VvazU5bmIifQ.E1EL215k2CB7mwfS-LIXAA",
     GOOGLE_CLIENT_ID:
-      "95071508636-t1fm1jtgi18nbumlh44qb3eg9fi1btej.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "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",
+      "543078423507-22ebbs5qjqo3a7blgq9cvkldi18353fh.apps.googleusercontent.com",
+    GOOGLE_CLIENT_SECRET: "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",
     NEXTAUTH_URL: "http://locahost:3001",
-    JWT_SECRET: "de2b429805c96302d77595caffcbf218"
-
+    JWT_SECRET: "de2b429805c96302d77595caffcbf218",
   },
   async rewrites() {
     return [
